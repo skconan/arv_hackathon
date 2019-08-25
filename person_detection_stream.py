7c85428812ff2c40f11bc81f4eabb661863904e9
skconan <supakit.kr@gmail.com>
2019-08-25 11:22:31 +0700

    before testing

--- person_detection_stream.py
+++ person_detection_stream.py
@@ -6,21 +6,34 @@ import cv2 as cv
 from constants import *
 from utilities import *
 from keras.models import load_model
+from submission import submission
+import time
 
 model_file = MODEL_PATH + r"/model-color-obj-bg.hdf5"
 model = load_model(model_file)
+reponse_payload = {
+    "scene_no": 1,
+    "ppe": {
+        "helmet": False,
+        "glasses": False,
+        "coverall": False,
+        "boots": False,
+        "gloves": False
+    }
+}
+
+def check_is_object(binary):
+    r,c = binary.shape
+    white = np.count_nonzero(binary)
+    ratio = white/(r*c)
+    return ratio >= 0.4
 
 def object_detection(img):
     height, width, _ = img.shape
-    check = {
-             'helmet':False,
-             'coverall':False,
-             'boots':False,
-             'groove':False,
-             }
     
     lower_bound = {
         'helmet': 0,
+        'glasses': 0,
         'coverall': int(height*0.15),
         'boots': int(height*0.75),
         'groove': int(height*0.25),
@@ -31,38 +44,30 @@ def object_detection(img):
         'boots': int(height),
         'groove': int(height*0.7),
     }
-    color = {
-        'helmet': {
-          'lower': np.array([127,127,127],np.uint8),
-          'upper': np.array([255,255,255],np.uint8)
-        },
-        'coverall': {
-          'lower': np.array([127,127,127],np.uint8),
-          'upper': np.array([255,255,255],np.uint8)
-        },
-        'boots': (255, 0, 0),
-        'groove': (0, 255, 0),
-    }
+    
 
-    # helmet = img.copy()[:,:]
     result = img.copy()
     for k in lower_bound.keys():
         lower = lower_bound[k]
         upper = upper_bound[k]
-        # cv.rectangle(result, (0, lower), (width, upper), color[k], 2)
-        roi = img.copy()[:width,lower:upper]
-        hsv = cv.cvtColor(roi.copy(),cv.COLOR_BGR2HSV)
-        color_th = cv.inRange(hsv.copy(), lower)
-    cv.imshow('result_divide', result)
-    cv.waitKey(1)
+        roi = img.copy()[:width, lower:upper]
+        
+        hsv = cv.cvtColor(roi.copy(), cv.COLOR_BGR2HSV)
+        _,color_th = cv.inRange(hsv.copy(), COLOR_SEGMENT[k]['lower'], COLOR_SEGMENT[k]['upper'])
+        is_obj = check_is_object(color_th)
 
+        if is_obj:
+            cv.rectangle(result, (0,lower), (width, upper))
+        update_payload()        
+    
 
 def color_detection(img):
-    lower = [160,0,0]
-    upper = [179, 255,255]
+    lower = [160, 0, 0]
+    upper = [179, 255, 255]
     hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
-    lower = np.array(lower,np.uint8)
-    upper = np.array(upper,np.uint8)
+    lower = np.array(lower, np.uint8)
+    upper = np.array(upper, np.uint8)
+
 
 def capture():
     name = "out.avi"
@@ -74,73 +79,83 @@ def capture():
             break
 
 
-
 def predict(image):
     global model
-    rows,cols,ch = image.shape
+    rows, cols, ch = image.shape
     frame = image.copy()
     frame = cv.cvtColor(frame.copy(), cv.COLOR_BGR2RGB)
-    frame = cv.resize(frame,(256,256))
-    frame = frame.reshape((1,256,256,3))
+    frame = cv.resize(frame, (256, 256))
+    frame = frame.reshape((1, 256, 256, 3))
     frame = frame.astype('float32')
     frame = (frame / 255.)
     pred = model.predict(frame)[0]
-        
-    pred = cv.resize(pred.copy(), (cols,rows))	
+
+    pred = cv.resize(pred.copy(), (cols, rows))
     pred = cv.cvtColor(pred.copy(), cv.COLOR_RGB2BGR)
     pred = pred * 255.
     pred = pred.astype('uint8')
     return pred
 
+
+
+def update_payload(name):
+    global reponse_payload
+    reponse_payload['ppe'][name] = True
+
 def main():
-    vdo_path = r"D:\hackathon\hackathon_image_streaming\videos"
-    # bg = cv.imread(IMG_PATH + "/bg.jpg",0)
-    # bg = cv.GaussianBlur(bg, (11, 11), 0)
-    name = "out_all_forward.avi"
-    name = "out_all_no_hand.avi"
-    # vdo_name = 
-    # cap = cv.VideoCapture()
-    cap = cv.VideoCapture(VDO_PATH + "/" + name)
+    start_time = time.time()
+
+    print("Socket initialize")
+    socket = socket_setup()
+    print("Socket setup successful")
+
     min_area = (FRAME_H * FRAME_W)*0.04
-    # back_sub = cv.createBackgroundSubtractorKNN()
     get_bg = False
-    frame_count = 0
+
     while True:
         try:
-            ret, img = cap.read()
-            if not ret:
+            _, image = recv_image(socket)
+            img = np.array(image)[:, :, ::-1]
+
+            if img is None:
                 print("Image is None")
                 break
         except:
             continue
+
         if not get_bg:
             bg = img.copy()
-            bg = cv.cvtColor(bg,cv.COLOR_BGR2GRAY)
+            bg = cv.cvtColor(bg, cv.COLOR_BGR2GRAY)
             get_bg = True
             continue
-        
+
         # fg = backSub.apply(frame)
         obj = bg_subtraction(img, bg.copy(), mode='neg')
         person = cv.bitwise_and(img, img, mask=obj)
 
-        _,th = cv.threshold(obj, 127, 255, cv.THRESH_BINARY)
-        # th = cv.erode(th,get_kernel(ksize=(5,5)))
-        th = cv.dilate(th,get_kernel())
-        cv.imshow("th",th)
-        _,contours,_ = cv.findContours(th,cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
-        frame_move = 0
+        _, th = cv.threshold(obj, 127, 255, cv.THRESH_BINARY)
+        th = cv.dilate(th, get_kernel())
+        _, contours, _ = cv.findContours(
+            th, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
         result = img.copy()
-        cv.drawContours(result,contours,-1,(255,0,0),2)
+        cv.drawContours(result, contours, -1, (255, 0, 0), 2)
+
         for cnt in contours:
             area = cv.contourArea(cnt)
             if area < min_area:
                 continue
             (x, y, w, h) = cv.boundingRect(cnt)
             if w > FRAME_W * 0.3:
+                print("Continues...")
+                print("Width of object is too large")
                 continue
             if h < FRAME_H*0.5:
+                print("Continues...")
+                print("Height of object is too short")
                 continue
             if h/w < 1.5:
+                print("Continues...")
+                print("Height/Width Ratio is not ok")
                 continue
             # wh_ratio = 1.*w/h
             add_h = h*0.25
@@ -151,19 +166,20 @@ def main():
             cv.rectangle(result, (x, y), (x + w, y + h), (0, 255, 0), 2)
             roi = img[y:y+h, x:x+w]
             predicted = predict(roi)
-        frame_count += 1
-        # if frame_count >= 30:
-        #     frame_count = 0
-        #     get_bg = False
+            payload = object_detection(predicted)
+            update_payload(payload)
+            if time.time() - start_time > TIMEOUT:
+                submission(reponse_payload,is_test=True)
+
+
         cv.imshow("obj", obj)
-        # cv.imshow("img", img)
-        # cv.imshow("bg", bg)
         cv.imshow("person", person)
         cv.imshow("result", result)
-        k = cv.waitKey(100) & 0xff
+        k = cv.waitKey(1) & 0xff
+
         if k == ord('q'):
             break
-    cap.release()
+
 
 if __name__ == "__main__":
     main()
